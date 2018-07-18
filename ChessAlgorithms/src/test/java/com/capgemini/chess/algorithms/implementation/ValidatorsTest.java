64122fcc76dcfad3214106fbb1edf011ce806110
JakubMitulski <jakub.mitulski@gmail.com>
2018-07-18 09:30:02 +0200

    Added test for KingValidator

--- ChessAlgorithms/src/test/java/com/capgemini/chess/algorithms/implementation/ValidatorsTest.java
+++ ChessAlgorithms/src/test/java/com/capgemini/chess/algorithms/implementation/ValidatorsTest.java
@@ -6,6 +6,7 @@ import com.capgemini.chess.algorithms.data.enums.Color;
 import com.capgemini.chess.algorithms.data.enums.Piece;
 import com.capgemini.chess.algorithms.data.generated.Board;
 import com.capgemini.chess.algorithms.implementation.validators.BishopValidator;
+import com.capgemini.chess.algorithms.implementation.validators.KingValidator;
 import org.junit.Test;
 
 import java.util.Set;
@@ -27,5 +28,20 @@ public class ValidatorsTest {
 
         //Then
         assertEquals(8, allBishopMoves.size());
+    }
+
+    @Test
+    public void shouldGetAllPossibleMovesOfKing(){
+        //Given
+        Board board = new Board();
+        board.setPieceAt(Piece.BLACK_KING, new Coordinate(3, 0));
+        board.setPieceAt(Piece.WHITE_PAWN, new Coordinate(2, 1));
+        board.setPieceAt(Piece.BLACK_PAWN, new Coordinate(3, 1));
+
+        //When
+        Set<Move> allKingMoves = new KingValidator(new Coordinate(3, 0), board, Color.BLACK).getMoves();
+
+        //Then
+        assertEquals(4, allKingMoves.size());
     }
 }
